b73defa69db386e97ab653175ed854865d661b1f
toyobayashi <lifenglin314@outlook.com>
2020-03-06 15:43:50 +0800

    header only console

--- include/console.hpp
+++ include/console.hpp
@@ -22,14 +22,58 @@
 #include <vector>
 #include <map>
 #include <iostream>
-#include "charset.hpp"
 
 namespace toyo {
+
+namespace {
+  std::string a2ocp(const std::string& str) {
+#ifdef _WIN32
+    int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+    if (len == 0) {
+      return "";
+    }
+    wchar_t* wstr = new wchar_t[len];
+    memset(wstr, 0, len * sizeof(wchar_t));
+    len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, wstr, len);
+    if (len == 0) {
+      delete[] wstr;
+      return "";
+    }
+
+    UINT cp = GetConsoleOutputCP();
+    len = WideCharToMultiByte(cp, 0, wstr, -1, nullptr, 0, nullptr, nullptr);
+    if (len == 0) {
+      delete[] wstr;
+      return "";
+    }
+    char* buf = new char[len];
+    memset(buf, 0, len * sizeof(char));
+    len = WideCharToMultiByte(cp, 0, wstr, -1, buf, len, nullptr, nullptr);
+    if (len == 0) {
+      delete[] wstr;
+      delete[] buf;
+      return "";
+    }
+    std::string res(buf);
+    delete[] wstr;
+    delete[] buf;
+    return res;
+#else
+    return str;
+#endif
+  }
+}
 
 class console {
 private:
 #ifdef _WIN32
-  static WORD _set_console_text_attribute(HANDLE, WORD);
+  static WORD _set_console_text_attribute(HANDLE hConsole, WORD wAttr) {
+    CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+    if (!GetConsoleScreenBufferInfo(hConsole, &csbiInfo)) return FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
+    WORD originalAttr = csbiInfo.wAttributes;
+    SetConsoleTextAttribute(hConsole, wAttr);
+    return originalAttr;
+  }
 #endif
 
   static std::string _format(char);
@@ -78,7 +122,7 @@ private:
 
   template <typename... Args>
   static void _logerror(const std::string& format, Args... args) {
-    std::string f = toyo::charset::a2ocp(format) + "\n";
+    std::string f = a2ocp(format) + "\n";
     fprintf(stderr, f.c_str(), args...);
   }
 
@@ -97,7 +141,7 @@ public:
 
   template <typename... Args>
   static void write(const std::string& format, Args... args) {
-    printf(toyo::charset::a2ocp(format).c_str(), args...);
+    printf(a2ocp(format).c_str(), args...);
   }
 
   template <typename T>
@@ -111,7 +155,7 @@ public:
 
   template <typename... Args>
   static void log(const std::string& format, Args... args) {
-    std::string f = toyo::charset::a2ocp(format) + "\n";
+    std::string f = a2ocp(format) + "\n";
     printf(f.c_str(), args...);
   }
 
@@ -165,6 +209,166 @@ public:
   static void clear();
   static void clear_line(short lineNumber = 0);
 }; // class console
+
+inline std::string console::_format(char c) {
+  char buf[2] = { c, '\0' };
+  return std::string(buf);
+}
+
+inline std::string console::_format(bool b) {
+  return b ? "true" : "false";
+}
+
+inline std::string console::_format(const char* cstr) {
+  return a2ocp(cstr);
+}
+
+inline std::string console::_format(const std::vector<unsigned char>& buf) {
+  char _map[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+  std::ostringstream oss;
+  oss << "<Buffer ";
+  for (size_t i = 0; i < buf.size(); i++) {
+    oss << _map[buf[i] >> 4];
+    oss << _map[buf[i] & 0x0f];
+    if (i != buf.size() - 1) {
+      oss << " ";
+    }
+  }
+  oss << ">";
+  return oss.str();
+}
+
+inline std::string console::_format(const std::vector<std::string>& arr) {
+  size_t len = arr.size();
+  std::ostringstream oss;
+  if (len == 0) {
+    oss << "[]";
+    return oss.str();
+  }
+  oss << "[ ";
+  for (size_t i = 0; i < len; i++) {
+    oss << "\"" << a2ocp(arr[i]) << "\"";
+    if (i != len - 1) {
+      oss << ", ";
+    }
+  }
+  oss << " ]";
+  return oss.str();
+}
+
+inline std::string console::_format(const std::map<std::string, std::string>& strobj) {
+  size_t len = strobj.size();
+  std::ostringstream oss;
+  if (len == 0) {
+    oss << "{}";
+    return oss.str();
+  }
+  oss << "{\n";
+  size_t i = 0;
+  for (auto& p : strobj) {
+    oss << a2ocp(std::string("  \"") + p.first + "\": \"" + p.second + "\"");
+    if (i != len - 1) {
+      oss << ",\n";
+    } else {
+      oss << "\n";
+    }
+    i++;
+  }
+  oss << "}";
+  return oss.str();
+}
+
+inline std::string console::_format(const std::string& str) {
+  return a2ocp(str);
+}
+
+inline void console::_logerror(const char* arg) {
+  std::cerr << _format(arg) << std::endl;
+}
+
+inline void console::_logerror(const std::string& arg) {
+  std::cerr << _format(arg) << std::endl;
+}
+
+inline void console::log(const char* arg) {
+  std::cout << _format(arg) << std::endl;
+}
+
+inline void console::log(const std::string& arg) {
+  std::cout << _format(arg) << std::endl;
+}
+
+inline void console::write(const char* arg) {
+  std::cout << _format(arg);
+}
+
+inline void console::write(const std::string& arg) {
+  std::cout << _format(arg);
+}
+
+inline void console::clear() {
+#ifdef _WIN32
+  HANDLE _consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD coordScreen = { 0, 0 };
+  DWORD cCharsWritten;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  DWORD dwConSize;
+
+  if (!GetConsoleScreenBufferInfo(_consoleHandle, &csbi)) {
+    return;
+  }
+
+  dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+
+  if (!FillConsoleOutputCharacter(_consoleHandle,
+    (TCHAR) ' ',
+    dwConSize,
+    coordScreen,
+    &cCharsWritten))
+  {
+    return;
+  }
+
+  if (!GetConsoleScreenBufferInfo(_consoleHandle, &csbi)) {
+    return;
+  }
+
+  if (!FillConsoleOutputAttribute(_consoleHandle,
+    csbi.wAttributes,
+    dwConSize,
+    coordScreen,
+    &cCharsWritten))
+  {
+    return;
+  }
+
+  SetConsoleCursorPosition(_consoleHandle, coordScreen);
+#else
+  std::cout << "\033[2J\033[1;1H";
+#endif
+}
+
+inline void console::clear_line(short lineNumber) {
+#ifdef _WIN32
+  HANDLE _consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  if (!GetConsoleScreenBufferInfo(_consoleHandle, &csbi)) return;
+  short tmp = csbi.dwCursorPosition.Y - lineNumber;
+  COORD targetFirstCellPosition = { 0, tmp < 0 ? 0 : tmp };
+  DWORD size = csbi.dwSize.X * (lineNumber + 1);
+  DWORD cCharsWritten;
+
+  if (!FillConsoleOutputCharacterW(_consoleHandle, L' ', size, targetFirstCellPosition, &cCharsWritten)) return;
+  if (!GetConsoleScreenBufferInfo(_consoleHandle, &csbi)) return;
+  if (!FillConsoleOutputAttribute(_consoleHandle, csbi.wAttributes, size, targetFirstCellPosition, &cCharsWritten)) return;
+  SetConsoleCursorPosition(_consoleHandle, targetFirstCellPosition);
+#else
+  for (short i = 0; i < lineNumber; i++) {
+    std::cout << "\x1b[666D\x1b[0K\x1b[1A";
+  }
+  std::cout << "\x1b[666D\x1b[0K";
+#endif
+}
 
 } // namespace toyo
 
